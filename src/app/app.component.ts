9fd20f176adb9ef96cf21317907f1a512741ad3c
Dan Mace <dmacebeta@gmail.com>
2018-10-16 23:05:27 -0700

    practice2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,22 @@ export class AppComponent {
   title = 'To Do It';
   currentFocus: string = 'Angular Homework';
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+  selectedTask: Task = this.tasks[0];
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+  priorityColor(currentTask){
+    if (parseInt(currentTask.priority) === 3){
+      return "bg-danger";
+    } else if (parseInt(currentTask.priority) === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
